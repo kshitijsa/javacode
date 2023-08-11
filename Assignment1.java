45435f2f1e588bfcced7c8dc52c8466e227cd8c9
kshitijsa <130733481+kshitijsa@users.noreply.github.com>
2023-08-11 12:16:43 +0530

    Print 1to n natural numbers using while loop

---
+++ Assignment1.java
@@ -0,0 +1,18 @@
+//kshitij sahu
+//Rollno-->2215000952
+
+import java.util.Scanner;
+
+public class Assignment1 { 
+      public static void main(String args[]) {
+        Scanner ob = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the limit that is value of n");
+        n = ob.nextInt();
+        int i = 1;
+        while (i <= n) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
