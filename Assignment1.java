f6cd4b58fbf7be9dae4a42ae0008c1903d168354
kshitijsa <130733481+kshitijsa@users.noreply.github.com>
2023-08-11 12:24:00 +0530

    Find the number is divisible by 6 and 7 both

--- Assignment1.java
+++ Assignment1.java
@@ -1,13 +1,17 @@
 //kshitij sahu
 //Rollno-->2215000952
-
 import java.util.*;
-  public static void main(String args[]) {
+public class Assignment1{
+    public static void main(String args[]) {
         Scanner ob = new Scanner(System.in);
-        int n, i;
         System.out.println("Enter the number");
-        n = ob.nextInt();
-        for (i = 1; i <= n; i++) {
-            System.out.println(i);
- }}
+        int a = ob.nextInt();
+        if (a % 6 == 0) {
+            if (a % 7 == 0)
+                System.out.println("YES");
+        } 
+        else
+            System.out.println("NO");
+}
+}
 
